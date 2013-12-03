e7c213d9f1331375ddab2d0de10d4a60fead6442
Anton Khlynovskiy <subzey@gmail.com>
2013-12-03 15:33:28 +0400

    574 bytes; escape & in templater

--- src/make.node.js
+++ src/make.node.js
@@ -7,7 +7,7 @@ var annotatedBuffer = require('fs').readFileSync('./annotated.html');
 
 html = html.replace(/\{\{crushed_count\}\}/g, crushedBuffer.length);
 html = html.replace(/\{\{raw_count\}\}/g, rawBuffer.length);
-html = html.replace(/\{\{raw_escaped\}\}/g, rawBuffer.toString('utf-8').replace(/</g, '&lt;'));
+html = html.replace(/\{\{raw_escaped\}\}/g, rawBuffer.toString('utf-8').replace(/&/g, '&amp;').replace(/</g, '&lt;'));
 html = html.replace(/\{\{annotated\}\}/g, annotatedBuffer.toString('utf-8'));
 html = html.replace(/\{\{crushed\}\}/g, crushedBuffer.toString('utf-8'));
 
